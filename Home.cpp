43666dae32341230000c4f19377deee44c49188a
Hanif Eridaputra <he.23292@gmail.com>
2013-03-25 02:21:13 +0700

    nih

--- Home.cpp
+++ Home.cpp
@@ -4,40 +4,7 @@
 #define HOME_WIDTH 20
 
 Home::Home(){
-	cout << "masuk" << endl;
-	
-//	Screen _s(HOME_WIDTH,HOME_HEIGHT);
-	/*_s.drawScreen(1);*/
-/*	screenHome = _s;
-	
-	char subs;
-	for (int i=0; i<4; i++){
-		switch (i) {
-			
-			case 0 : {
-				subs = 'E';
-				break;
-			}
-			
-			case 1 : {
-				subs = 'M';
-				break;
-			}
-			
-			case 2 : {
-				subs = 'O';
-				break;
-			}
-			
-			case 3 : {
-				subs = 'H';
-				break;
-			}
-		}
-		
-		//_s.matriks[HOME_WIDTH-i,i] = subs;
-		screenHome.setMatriks(0,HOME_WIDTH-i-1,subs);
-	} */
+	cout << "Masuk" << endl;
 }
 
 Home::~Home(){
@@ -45,5 +12,5 @@ Home::~Home(){
 }
 
 void Home::drawHome(){
-	this->screenHome.drawScreen(1);
+	this->screenHome.drawScreen(0);
 }
